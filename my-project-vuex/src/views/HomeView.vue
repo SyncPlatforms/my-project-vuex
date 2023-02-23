1a15cbbb2e5da94d9522ea8df696a43da35cf89b
SyncPlatforms <120284398+SyncPlatforms@users.noreply.github.com>
2023-02-23 15:07:44 +0330

    L3-end

---
+++ my-project-vuex/src/views/HomeView.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<script>
+import EventCard from '@/components/EventCard.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    EventCard,
+  },
+  data() {
+    return {
+      events: [
+        {
+          id: 123456,
+          category: 'animal welfare',
+          title: 'Cat Adoption Day',
+          description: 'Find your new feline at this event',
+          location: 'Tehran',
+          date: 'November 28, 2022',
+          time: '12:00',
+          petsAllowed: true,
+          organizer: 'Kat Laydee',
+        },
+        {
+          id: 123456,
+          category: 'animal welfare',
+          title: 'Cat Adoption Day',
+          description: 'Find your new feline at this event',
+          location: 'Tehran',
+          date: 'November 28, 2022',
+          time: '12:00',
+          petsAllowed: true,
+          organizer: 'Kat Laydee',
+        },
+        {
+          id: 123456,
+          category: 'animal welfare',
+          title: 'Cat Adoption Day',
+          description: 'Find your new feline at this event',
+          location: 'Tehran',
+          date: 'November 28, 2022',
+          time: '12:00',
+          petsAllowed: true,
+          organizer: 'Kat Laydee',
+        },
+        {
+          id: 123456,
+          category: 'animal welfare',
+          title: 'Cat Adoption Day',
+          description: 'Find your new feline at this event',
+          location: 'Tehran',
+          date: 'November 28, 2022',
+          time: '12:00',
+          petsAllowed: true,
+          organizer: 'Kat Laydee',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
